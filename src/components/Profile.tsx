ede28698b8f192be55a9dfab601c36e56405becb
Wilson Faustino <wilson.rfaustino@gmail.com>
2021-02-26 00:13:26 -0300

    first commit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../context/ChallengesContext';
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+  const {level} = useContext(ChallengesContext);
+
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/wilsonfaustino.png" alt="Wilson Faustino" />
+      <div>
+        <strong>Wilson Faustino</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+                    Level {level}
+                </p>
+      </div>
+    </div>
+  )
+}
